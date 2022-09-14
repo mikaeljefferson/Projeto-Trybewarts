b341588f284ca0789689eda2880bc03aee754608
mikael jefferson da silva <mikaeljeffeson@gmail.com>
2022-09-14 17:40:14 -0300

    quesistos do 1 ao 6

---
+++ script.js
@@ -0,0 +1,11 @@
+const buttonLogin = document.getElementById('btn-sub');
+const emailUser = document.getElementById('userEmail');
+const pass = document.getElementById('userPassword');
+buttonLogin.addEventListener('click', (event) => {
+    event.preventDefault();
+    if(emailUser.value === "tryber@teste.com" && pass.value === "123456"){
+        alert("Olá, Tryber!");
+    } else {
+        alert("Email ou senha inválidos.");
+    }
+});
