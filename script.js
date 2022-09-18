80eeb7477bea791b23454d2d95556400bd7e35dd
mikael jefferson da silva <mikaeljeffeson@gmail.com>
2022-09-18 10:31:09 -0300

    linter

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const buttonLogin = document.getElementById('btn-sub');
 const emailUser = document.getElementById('userEmail');
 const pass = document.getElementById('userPassword');
 buttonLogin.addEventListener('click', (event) => {
-    event.preventDefault();
-    if(emailUser.value === "tryber@teste.com" && pass.value === "123456"){
-        alert("Olá, Tryber!");
-    } else {
-        alert("Email ou senha inválidos.");
-    }
+  event.preventDefault();
+  if (emailUser.value === 'tryber@teste.com' && pass.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
 });
